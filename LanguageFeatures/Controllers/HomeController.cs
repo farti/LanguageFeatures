4d15b13e0dad419875d5a903f3f48a4666a6aa20
Marcin Rakoczy <mrakoczy@wp.pl>
2017-11-06 14:00:22 +0100

    cz1

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Przejście do adresu URL pokazującego przykład:";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            // utworzenie nowego obiektu Product
+            Product myProduct = new Product();
+
+            // ustawienie wartości właściwości
+            myProduct.Name = "Kajak";
+
+            // odczytanie właściwości
+            string productName = myProduct.Name;
+
+            // wygenerowanie widoku
+            return View("Result", (object)String.Format("Nazwa produktu: { 0}", productName));
+        }
+    }
+}
